c1cc360768e3fd59c680fe486f281ddba2b1b174
Danny Sullivan <dbsullivan23@gmail.com>
2015-04-09 12:27:34 +0200

    adding selection of newline for duplication

--- select_above.py
+++ select_above.py
@@ -3,19 +3,24 @@ import sublime, sublime_plugin
 class SelectAboveCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
 
+		# get the current start and end of the selection
 		curr_1 = self.view.sel()[0].begin()
 		curr_2 = self.view.sel()[-1].end()
+
+		#get the current row and column of the start and end of the selection
 		row_1, col_1 = self.view.rowcol(curr_1)
 		row_2, col_2 = self.view.rowcol(curr_2)
-		if (col_1 == 0 and col_1 != col_2 and row_1 != 0):
-			 row = row_1 - 1
+
+		# if we are at the beginning of the line with a selection and we are
+		# not at the very beginning of the file, select the line above
+		# otherwise we want to just highlight the first line
+		if (col_1 == 0 and (col_1 != col_2 or row_1 != row_2) and row_1 != 0):
+			row_to = row_1 - 1
 		else:
-			row = row_1
-		line = self.view.line(curr_1)
-		end_row, end_col = self.view.rowcol(line.end())
-		my_from = self.view.text_point(row , end_col)
-		my_to = self.view.text_point(row, 0)
-		line = self.view.line(self.view.text_point(5, 3))
+			row_to = row_1
+
+		my_from = self.view.text_point(row_1 + 1, 0)
+		my_to = self.view.text_point(row_to, 0)
 		cur_region = self.view.sel()[0]
 		self.view.sel().clear()
 		self.view.sel().add(cur_region.cover(sublime.Region(my_from, my_to)))
